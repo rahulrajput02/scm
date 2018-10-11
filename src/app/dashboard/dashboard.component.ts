e75305c584b27ee14992b85984e921b4a5e4c794
rahulrajput02 <rahul_king_may2@yahoo.com>
2018-10-11 15:04:03 +0530

    updated Ui

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -19,6 +19,8 @@ export class dashboardComponent {
   connection;
   dashboardData;
 
+
+
   constructor(private fb: FormBuilder, private httpClient: HttpClient, private _chart: ChartService) {
   }
 
@@ -28,7 +30,7 @@ export class dashboardComponent {
       id: '3324',
       type: 'Shoe laces',
       items: '500',
-      status: 'Dispatched',
+      status: 'Recieved',
       lastUpdated: '02/10/2018, 1:34:23 pm'
     },
     {
@@ -63,48 +65,40 @@ export class dashboardComponent {
 
     this.dashboardData = dataGet;
 
-    this._chart.dailyForecast()
-      .subscribe(res => {
-        let temp_max = res['list'].map(res => res.main.temp_max);
-        let temp_min = res['list'].map(res => res.main.temp_min);
-        let alldates = res['list'].map(res => res.dt)
 
-        let weatherDates = []
-        alldates.forEach((res) => {
-          this.chart = new Chart('canvas', {
-            type: 'bar',
-            data: {
-              labels: ["Waiting", "Confirmed", "Dispached", "Recieved"],
-              datasets: [{
-                label: 'Number of Orders',
-                data: [12, 19, 3, 8],
-                backgroundColor: [
-                  'rgba(255, 99, 132, 0.7)',
-                  'rgba(54, 162, 235, 0.7)',
-                  'rgba(255, 206, 86, 0.7)',
-                  'rgba(75, 192, 192, 0.7)'
-                ],
-                borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)'
-                ],
-                borderWidth: 1
-              }]
-            },
-            options: {
-              scales: {
-                yAxes: [{
-                  ticks: {
-                    beginAtZero: true
-                  }
-                }]
-              }
+
+    this.chart = new Chart('canvas', {
+      type: 'bar',
+      data: {
+        labels: ["Waiting", "Confirmed", "Dispached", "Recieved"],
+        datasets: [{
+          label: 'Orders Status',
+          data: [12, 19, 3, 8],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.7)',
+            'rgba(75, 192, 192, 0.7)',
+            'rgba(255, 206, 86, 0.7)',
+            'rgba(54, 162, 235, 0.7)',
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(54, 162, 235, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
             }
-          });
-        })
-      })
+          }]
+        }
+      }
+    });
   }
 
   validate() {
@@ -113,5 +107,44 @@ export class dashboardComponent {
       "Timestamp": '18-09-2018 03:14:07'
     }
     alert(JSON.stringify(validateObj, null, "\t"));
+  }
+
+  all() {
+    this.ngOnInit();
+  }
+
+  recieved() {
+    var dataGet = [
+      {
+        id: '3324',
+        type: 'Shoe laces',
+        items: '500',
+        status: 'Recieved',
+        lastUpdated: '02/10/2018, 1:34:23 pm'
+      },
+      {
+        id: '1236',
+        type: 'Leather',
+        items: '240',
+        status: 'Recieved',
+        lastUpdated: '15/10/2018, 11:10:18 pm'
+      }
+    ]
+
+    this.dashboardData = dataGet;
+  }
+
+  waiting() {
+    var dataGet = [
+      {
+        id: '7635',
+        type: 'Shoe laces',
+        items: '540',
+        status: 'Waiting',
+        lastUpdated: '12/10/2018, 10:14:29 pm'
+      }
+    ]
+
+    this.dashboardData = dataGet;
   }
 }
