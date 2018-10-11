6286360a98b9f4353f7519f211e0fd4f9541e9c5
rahulrajput02 <rahul_king_may2@yahoo.com>
2018-10-11 16:02:49 +0530

    Added createpackage functionality

--- ui/src/app/createPackage/createPackage.component.ts
+++ ui/src/app/createPackage/createPackage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators, NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Router } from '@angular/router';
@@ -16,24 +17,41 @@ export class createPackageComponent {
     transactionData;
     fileUrl;
 
+    angularForm = new FormGroup({
+        orderType: new FormControl(),
+        currentOwner: new FormControl(),
+        quantityType: new FormControl(),
+        quantity: new FormControl()
+    });
 
-    constructor(private httpClient: HttpClient, private routes: Router) { }
-
-
-    ngOnInit() {
-        this.validate = false;
-
-        this.httpClient.get(environment.getDataFromDB)
-            .subscribe(
-                response => {
-                    console.log(response);
-                    this.fillingData = response;
-                },
-                err => {
-                    console.log("Error Ocurred" + err);
-                }
-            )
+    constructor(private fb: FormBuilder, private httpClient: HttpClient, private routes: Router) {
+        this.createForm();
     }
+
+    createForm() {
+        this.angularForm = this.fb.group({
+            orderType: ['', Validators.required],
+            currentOwner: ['', Validators.required],
+            quantityType: ['', Validators.required],
+            quantity: ['', Validators.required]
+        });
+    }
+
+
+    // ngOnInit() {
+    //     this.validate = false;
+
+    //     this.httpClient.get(environment.getDataFromDB)
+    //         .subscribe(
+    //             response => {
+    //                 console.log(response);
+    //                 this.fillingData = response;
+    //             },
+    //             err => {
+    //                 console.log("Error Ocurred" + err);
+    //             }
+    //         )
+    // }
 
     validateButton(transactionId) {
         this.httpClient.get(environment.getTransactionDetails + transactionId)
@@ -46,32 +64,74 @@ export class createPackageComponent {
             )
     }
 
-    pdfDownload(pdfPath) {
-        console.log(pdfPath);
-        const data = { "Pdf_Path": pdfPath };
-        this.httpClient.post(environment.postPdf, data, { responseType: 'text' })
-            .subscribe(response => {
-                console.log(response);
+    createPackage(event) {
+        const target = event.target;
 
-                this.httpClient.get(environment.getPdf, { responseType: 'arraybuffer' })
-                    .subscribe(response => {
-                        console.log(response);
-                        var blob = new Blob([response], { type: 'application/pdf' });
-                        this.fileUrl = window.URL.createObjectURL(blob);
+        const orderType = target.querySelector('#orderType').value;
+        const currentOwner = target.querySelector('#currentOwner').value;
+        const quantityType = target.querySelector('#quantityType').value;
+        const quantity = target.querySelector('#quantity').value;
 
-                        //Open PDF in new tab
-                        window.open(this.fileUrl);
+        const myobj = {
+            "Order_Type": orderType, "Current_Owner": currentOwner, "Quantity_Type": quantityType, "Quantity": quantity
+        };
 
-                        //***** Download PDF *****//
-                        // var link = document.createElement('a');
-                        // document.body.appendChild(link);
-                        // link.setAttribute('style', 'display: none');
-                        // link.href = this.fileUrl;
-                        // link.download = "fillingDocs.pdf";
-                        // link.click();
-                        // window.URL.revokeObjectURL(this.fileUrl);
-                        // link.remove();
-                    })
-            });
+        console.log(myobj);
+
+
+        // this.httpClient.post(environment.postNewFilling, myobj, { responseType: 'text' })
+        //     .subscribe(
+        //         response => {
+        //             console.log(response);
+        //             var hashFromResp = response;
+
+        //             var hashToBlock = {
+        //                 "$class": "org.example.mynetwork.NewFilling",
+        //                 "hashId": response
+        //             }
+
+        //             this.httpClient.post(environment.postToBlockChain, hashToBlock)
+        //                 .subscribe(
+        //                     response => {
+        //                         this.httpClient.get(environment.getNewFillingFromBlock + hashFromResp)
+        //                             .subscribe(
+        //                                 response => {
+        //                                     var submitTrans = {
+        //                                         "$class": "org.example.mynetwork.StoreHash",
+        //                                         "newFilling": "resource:org.example.mynetwork.NewFilling#" + response["hashId"],
+        //                                         "transactionId": "",
+        //                                         "timestamp": new Date()
+        //                                     }
+
+        //                                     this.httpClient.post(environment.postHashToBlock, submitTrans)
+        //                                         .subscribe(
+        //                                             response => {
+        //                                                 this.savedSuccess = true;
+        //                                                 this.saveState = false;
+        //                                                 this.validateBlock = response;
+        //                                                 console.log(response["transactionId"]);
+
+        //                                                 const objTran = { "transactionId": response["transactionId"] };
+        //                                                 this.httpClient.post(environment.postTransactionId, objTran, { responseType: 'text' })
+        //                                                     .subscribe(
+        //                                                         response => {
+        //                                                             console.log(response);
+        //                                                             // window.setInterval(reload, 2500);
+
+        //                                                             // function reload() {
+        //                                                             //   window.location.reload();
+        //                                                             // }
+        //                                                         }
+        //                                                     );
+        //                                             });
+        //                                 });
+        //                     }
+        //                 );
+        //         },
+
+        //         err => {
+        //             console.log("Error Ocurred" + err);
+        //         }
+        //     )
     }
 }
